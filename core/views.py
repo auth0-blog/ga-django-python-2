ff248c7d14c5a3007cb2d6e0118011c7f7473b4a
Kapehe <kapehe@Kapehes-MacBook-Pro.local>
2020-03-08 18:02:40 -0400

    initial commit

---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from .models import Post
+class PostListView(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        context = {"posts": posts}
+        return render(request, "home.html", context)
